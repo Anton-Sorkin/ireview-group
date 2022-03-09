784d87f6f5976e405319c64571ee41f2d5ce6ef5
Anton <anton.sorkin98@outlook.com>
2022-03-09 12:50:01 +0100

    added populate

--- models/EditsModels.js
+++ models/EditsModels.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require("mongoose");
 
-const editSchema = new mongoose.Schema({
-	favmovie: { type: String, required },
-	quote: { type: String, required },
-	quoteby: { type: String, required },
+const editSchema = new Schema({
+  favmovie: { type: String, required: true },
+  quote: { type: String, required: true },
+  quoteby: { type: String, required: true },
+  settingsBy: { type: Schema.Types.ObjectId, ref: "Users", required: true },
 });
 
 const EditsModel = model("Edits", editSchema);
--- routes/profilesRoute.js
+++ routes/profilesRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const utils = require("../utils/utils.js");
 const UsersModel = require("../models/UsersModels.js");
+const EditsModel = require("../models/EditsModels");
 const jwt = require("jsonwebtoken");
 
 const router = express.Router();
@@ -19,16 +20,33 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const user = await UsersModel.findById(req.params.id).lean();
-  res.render("profiles/single-copy", { user });
+  const settings = await EditsModel.find().lean();
+  res.render("profiles/single-copy", { user, settings });
 });
 
-// router.get("/edit-profile", (req, res) => {
-// 	res.render("profiles/edit-profiles");
-// });
-
 router.get("/edit-profile/:id", async (req, res) => {
+  const settings = await EditsModel.find().populate("settingsBy").lean();
+
   const user = await UsersModel.findById(req.params.id).lean();
-  res.render("profiles/edit-profiles", { user });
+  res.render("profiles/edit-profiles", { user, settings });
+  console.log(settings);
+});
+
+router.post("/edit-profile/:id", async (req, res) => {
+  const { favmovie, quote, quoteby, settingsBy } = req.body;
+
+  EditsModel.findOne({ favmovie }, async () => {
+    const newEdit = new EditsModel({
+      favmovie,
+      quote,
+      quoteby,
+      settingsBy,
+    });
+
+    await newEdit.save();
+
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
