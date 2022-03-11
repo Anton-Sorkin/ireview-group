c9574c66cb7f747ee97a31dff1d3ef13676ed800
Anton <anton.sorkin98@outlook.com>
2022-03-11 12:34:34 +0100

    first draft

--- models/MoviesModels.js
+++ models/MoviesModels.js
@@ -1,10 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const movieSchema = new Schema({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
-	genre: { type: String, required: true },
-	img: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: { type: String, required: true },
+  img: { type: String, required: true },
 });
 
 const MovieModel = model("Movies", movieSchema);
--- routes/profilesRoute.js
+++ routes/profilesRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const utils = require("../utils/utils.js");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const UsersModel = require("../models/UsersModels.js");
 const SettingsModel = require("../models/SettingsModels.js");
@@ -12,72 +13,75 @@ const { getUniqueFilename } = require("../utils/utils");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	const users = await UsersModel.find().lean();
+  const users = await UsersModel.find().lean();
 
-	res.render("profiles/profiles-list", { users });
+  res.render("profiles/profiles-list", { users });
 });
 
 router.get("/:id", async (req, res) => {
-	const user = await UsersModel.findById(req.params.id)
-		.populate("settings")
-		.lean();
+  const user = await UsersModel.findById(req.params.id)
+    .populate("settings")
+    .lean();
 
-	const reviews = await ReviewsModel.find({ reviewedBy: req.params.id })
-		.populate("reviewedTitle")
-		.populate("reviewedBy")
-		.lean();
-	const pic = await PictureModel.find({ picBy: req.params.id }).lean();
+  const reviews = await ReviewsModel.find({ reviewedBy: req.params.id })
+    .populate("reviewedTitle")
+    .populate("reviewedBy")
+    .lean();
+  const pic = await PictureModel.find({ picBy: req.params.id }).lean();
 
-	res.render("profiles/profiles-single", { user, reviews, pic });
+  res.render("profiles/profiles-single", { user, reviews, pic });
 });
 
 router.get("/edit-profile/:id", async (req, res) => {
-	const user = await UsersModel.findById(req.params.id)
-		.populate("settings")
-		.lean();
-	const pic = await PictureModel.find().populate("picBy").lean();
+  const user = await UsersModel.findById(req.params.id)
+    .populate("settings")
+    .lean();
+  const pic = await PictureModel.find().populate("picBy").lean();
 
-	console.log("USER\n", user.settings);
-	res.render("profiles/profiles-edit", { user, pic });
+  console.log("USER\n", user.settings);
+  res.render("profiles/profiles-edit", { user, pic });
 });
 
 router.post("/edit-profile/:id/:userId", async (req, res) => {
-	const { favmovie, quote, quoteby } = req.body;
+  const { favmovie, quote, quoteby } = req.body;
 
-	SettingsModel.findByIdAndUpdate(
-		req.params.id,
-		{
-			favmovie,
-			quote,
-			quoteby,
-		},
-		(error, docs, result) => {
-			if (error) res.status(500).redirect("/profiles");
-			else {
-				res.status(200).redirect("/profiles/" + req.params.userId);
-			}
-		}
-	);
+  SettingsModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      favmovie,
+      quote,
+      quoteby,
+    },
+    (error, docs, result) => {
+      if (error) res.status(500).redirect("/profiles");
+      else {
+        res.status(200).redirect("/profiles/" + req.params.userId);
+      }
+    }
+  );
 });
 
 router.post("/edit-profile-pic/:id", async (req, res) => {
-	const image = req.files.image;
-	const picBy = req.body.picBy;
+  const image = req.files.image;
+  const picBy = req.body.picBy;
 
-	const filename = getUniqueFilename(image.name);
-	const uploadPath = __dirname + "/../public/img/" + filename;
+  const filename = getUniqueFilename(image.name);
+  const uploadPath = __dirname + "/../public/img/" + filename;
 
-	await image.mv(uploadPath);
+  await image.mv(uploadPath);
 
-	const picture = new PictureModel({
-		name: req.body.name,
-		imageUrl: "/img/" + filename,
-		picBy,
-	});
+  const picture = new PictureModel({
+    name: req.body.name,
+    imageUrl: "/img/" + filename,
+    picBy,
+  });
+  if (utils.validatePicture(picture)) {
+    await picture.save();
+  } else {
+    console.log("error");
+  }
 
-	await picture.save();
-
-	res.redirect("/profiles");
+  res.redirect("/profiles");
 });
 
 module.exports = router;
