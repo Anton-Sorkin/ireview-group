6d289912757fd6ae7f1c42b38fab2354e06990c8
19OZMAN <76961722+19ozman@users.noreply.github.com>
2022-03-12 09:03:44 +0100

    tweaks

--- models/PictureModels.js
+++ models/PictureModels.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const pictureSchema = new Schema({
-  name: { type: String, required: false },
-  imageUrl: { type: String, required: true },
-  picBy: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+	name: { type: String, required: false },
+	imageUrl: { type: String, required: true },
+	picBy: { type: Schema.Types.ObjectId, ref: "Users", required: true },
 });
 
 const PictureModel = model("Pictures", pictureSchema);
